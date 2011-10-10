f052d56df18c15c19c686b6b3381b4606e942a92
Jennifer To <jenardto@gmail.com>
2011-10-10 02:02:32 -0700

    changed cube.obj, attempt at instancing groups :/

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -106,108 +106,223 @@ bool Scene::_parseLine(string line) {
     } else if (element[1] == 'i') {
       string instanceName;
       ss >> instanceName;
-      _currentPoly = new Polygon(instanceName); 
       string references;
       ss >> references;
-      Polygon * tempPoly = _polygons[references];
-      map<string, Vertex *> tempVertices = tempPoly->getVertices();
-      map<string, Face *> tempFaces = tempPoly->getFaces();
-      map<string, Vertex *> newVertices;
-      for (map<string, Vertex *>::iterator it = tempVertices.begin(); it != tempVertices.end(); it++) {
-	string vertName = (*it).first;
-	Vertex * tempVert = (*it).second;
-	vec3 pos = tempVert->getPos();
-	double x, y, z;
-	x = pos[0];
-	y = pos[1];
-	z = pos[2];
-	Vertex * addMe = new Vertex(x, y, z);
-        newVertices[vertName] = addMe;
-      }
-      while (!ss.eof()) {
-	string element;
-	ss >> element;
-	if (element[0] == '(') {
-	  if (element[1] == 't') {
-	    double x, y, z;
-	    ss >> x >> y >> z;
-	    for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
-	      string vertName = (*it).first;
-	      Vertex * tempVert = (*it).second;
-	      vec3 vertPos = tempVert->getPos();
-	      double nX, nY, nZ;
-	      nX = vertPos[0] + x;
-	      nY = vertPos[1] + y;
-	      nZ = vertPos[2] + z;
-	      tempVert->setPos(nX, nY, nZ);
-	    }
-	  } else if (element.substr(1,2) == "rx") {
-	    double angle;
-	    ss >> angle;
-	    angle = (angle*PI)/180;
-	    vec3 v1 = vec3(1, 0, 0);
-	    vec3 v2 = vec3(0, cos(angle), -sin(angle));
-	    vec3 v3 = vec3(0, sin(angle), cos(angle));
-	    mat3 rotX = mat3(v1, v2, v3);
-	    for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
-	      string vertName = (*it).first;
-	      Vertex * tempVert = (*it).second;
-	      vec3 vertPos = tempVert->getPos();
-	      vec3 newPos = rotX * vertPos;
-	      double nX, nY, nZ;
-	      nX = newPos[0];
-	      nY = newPos[1];
-	      nZ = newPos[2];
-	      tempVert->setPos(nX, nY, nZ);
-	    }
-	  } else if (element.substr(1,2) == "ry") {
-	    double angle;
-            ss >> angle;
-	    angle = (angle*PI)/180;
-            vec3 v1 = vec3(cos(angle), 0, sin(angle));
-            vec3 v2 = vec3(0, 1, 0);
-            vec3 v3 = vec3(-sin(angle), 0, cos(angle));
-            mat3 rotY = mat3(v1, v2, v3);
-            for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
-              string vertName = (*it).first;
-              Vertex * tempVert = (*it).second;
-              vec3 vertPos = tempVert->getPos();
-              vec3 newPos = rotY * vertPos;
-              double nX, nY, nZ;
-              nX = newPos[0];
-              nY = newPos[1];
-              nZ = newPos[2];
-              tempVert->setPos(nX, nY, nZ);
-	    }
-	  } else if (element.substr(1,2) == "rz") {
-	    double angle;
-	    ss >> angle;
-	    angle = (angle*PI)/180;
-	    vec3 v1 = vec3(cos(angle), -sin(angle), 0);
-	    vec3 v2 = vec3(sin(angle), cos(angle), 0);
-	    vec3 v3 = vec3(0, 0, 1);
-	    mat3 rotZ = mat3(v1, v2, v3);
-	    for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
-	      string vertName = (*it).first;
-	      Vertex * tempVert = (*it).second;
-	      vec3 vertPos = tempVert->getPos();
-	      vec3 newPos = rotZ * vertPos;
-	      double nX, nY, nZ;
-	      nX = newPos[0];
-	      nY = newPos[1];
-	      nZ = newPos[2];
-	      tempVert->setPos(nX, nY, nZ);
+      if (_polygons.find(references) != _polygons.end()) {
+        _currentPoly = new Polygon(instanceName);
+	Polygon * tempPoly = _polygons[references];
+	map<string, Vertex *> tempVertices = tempPoly->getVertices();
+	map<string, Face *> tempFaces = tempPoly->getFaces();
+	map<string, Vertex *> newVertices;
+	for (map<string, Vertex *>::iterator it = tempVertices.begin(); it != tempVertices.end(); it++) {
+	  string vertName = (*it).first;
+	  Vertex * tempVert = (*it).second;
+	  vec3 pos = tempVert->getPos();
+	  double x, y, z;
+	  x = pos[0];
+	  y = pos[1];
+	  z = pos[2];
+	  Vertex * addMe = new Vertex(x, y, z);
+	  newVertices[vertName] = addMe;
+	}
+	while (!ss.eof()) {
+	  string element;
+	  ss >> element;
+	  if (element[0] == '(') {
+	    if (element[1] == 't') {
+	      double x, y, z;
+	      ss >> x >> y >> z;
+	      for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
+		string vertName = (*it).first;
+		Vertex * tempVert = (*it).second;
+		vec3 vertPos = tempVert->getPos();
+		double nX, nY, nZ;
+		nX = vertPos[0] + x;
+		nY = vertPos[1] + y;
+		nZ = vertPos[2] + z;
+		tempVert->setPos(nX, nY, nZ);
+	      }
+	    } else if (element.substr(1,2) == "rx") {
+	      double angle;
+	      ss >> angle;
+	      angle = (angle*PI)/180;
+	      vec3 v1 = vec3(1, 0, 0);
+	      vec3 v2 = vec3(0, cos(angle), -sin(angle));
+	      vec3 v3 = vec3(0, sin(angle), cos(angle));
+	      mat3 rotX = mat3(v1, v2, v3);
+	      for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
+		string vertName = (*it).first;
+		Vertex * tempVert = (*it).second;
+		vec3 vertPos = tempVert->getPos();
+		vec3 newPos = rotX * vertPos;
+		double nX, nY, nZ;
+		nX = newPos[0];
+		nY = newPos[1];
+		nZ = newPos[2];
+		tempVert->setPos(nX, nY, nZ);
+	      }
+	    } else if (element.substr(1,2) == "ry") {
+	      double angle;
+	      ss >> angle;
+	      angle = (angle*PI)/180;
+	      vec3 v1 = vec3(cos(angle), 0, sin(angle));
+	      vec3 v2 = vec3(0, 1, 0);
+	      vec3 v3 = vec3(-sin(angle), 0, cos(angle));
+	      mat3 rotY = mat3(v1, v2, v3);
+	      for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
+		string vertName = (*it).first;
+		Vertex * tempVert = (*it).second;
+		vec3 vertPos = tempVert->getPos();
+		vec3 newPos = rotY * vertPos;
+		double nX, nY, nZ;
+		nX = newPos[0];
+		nY = newPos[1];
+		nZ = newPos[2];
+		tempVert->setPos(nX, nY, nZ);
+	      }
+	    } else if (element.substr(1,2) == "rz") {
+	      double angle;
+	      ss >> angle;
+	      angle = (angle*PI)/180;
+	      vec3 v1 = vec3(cos(angle), -sin(angle), 0);
+	      vec3 v2 = vec3(sin(angle), cos(angle), 0);
+	      vec3 v3 = vec3(0, 0, 1);
+	      mat3 rotZ = mat3(v1, v2, v3);
+	      for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
+		string vertName = (*it).first;
+		Vertex * tempVert = (*it).second;
+		vec3 vertPos = tempVert->getPos();
+		vec3 newPos = rotZ * vertPos;
+		double nX, nY, nZ;
+		nX = newPos[0];
+		nY = newPos[1];
+		nZ = newPos[2];
+		tempVert->setPos(nX, nY, nZ);
+	      }
 	    }
 	  }
 	}
-      }
-      for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
-	_currentPoly->addVertex((*it).first, (*it).second);
-      }
-      for (map<string, Face *>::iterator it = tempFaces.begin(); it != tempFaces.end(); it++) {
-	_currentPoly->addFace((*it).first, (*it).second);
-      }
-      _currentGroup->addPolygon(instanceName, _currentPoly);
+	for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
+	  _currentPoly->addVertex((*it).first, (*it).second);
+	}
+	for (map<string, Face *>::iterator it = tempFaces.begin(); it != tempFaces.end(); it++) {
+	  _currentPoly->addFace((*it).first, (*it).second);
+	}
+	_currentGroup->addPolygon(instanceName, _currentPoly);
+      } /*else if(_groups.find(references) != _groups.end()) {
+	Group * tempGroup = _groups[references];
+	map<string, Polygon *> tempPolygons = tempGroup->getPolygons();
+	for (map<string, Polygon*>::iterator ip = tempPolygons.begin(); ip != tempPolygons.end(); ip++) {
+	  string polyName = (*ip).first;
+	  Polygon * tempPoly = (*ip).second;
+	  Polygon * newPoly = new Polygon(polyName);
+	  map<string, Vertex *> tempVertices = tempPoly->getVertices();
+	  map<string, Face *> tempFaces = tempPoly->getFaces();
+	  map<string, Vertex *> newVertices;
+	  for (map<string, Vertex *>::iterator it = tempVertices.begin(); it != tempVertices.end(); it++) {
+	    string vertName = (*it).first;
+	    Vertex * tempVert = (*it).second;
+	    vec3 pos = tempVert->getPos();
+	    double x, y, z;
+	    x = pos[0];
+	    y = pos[1];
+	    z = pos[2];
+	    Vertex * addMe = new Vertex(x, y, z);
+	    newVertices[vertName] = addMe;
+	  }
+	  for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
+	    newPoly->addVertex((*it).first, (*it).second);
+	  }
+	  for (map<string, Face *>::iterator it = tempFaces.begin(); it != tempFaces.end(); it++) {
+	    newPoly->addFace((*it).first, (*it).second);
+	  }
+	  tempGroup->addPolygon(polyName, newPoly);
+	}
+	while (!ss.eof()) {
+	  string element;
+	  ss >> element;
+	  if (element[0] == '(') {
+	    if (element[1] == 't') {
+	      double x, y, z;
+	      ss >> x >> y >> z;
+	      for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
+		string vertName = (*it).first;
+		Vertex * tempVert = (*it).second;
+		vec3 vertPos = tempVert->getPos();
+		double nX, nY, nZ;
+		nX = vertPos[0] + x;
+		nY = vertPos[1] + y;
+		nZ = vertPos[2] + z;
+		tempVert->setPos(nX, nY, nZ);
+	      }
+	    } else if (element.substr(1,2) == "rx") {
+	      double angle;
+	      ss >> angle;
+	      angle = (angle*PI)/180;
+	      vec3 v1 = vec3(1, 0, 0);
+	      vec3 v2 = vec3(0, cos(angle), -sin(angle));
+	      vec3 v3 = vec3(0, sin(angle), cos(angle));
+	      mat3 rotX = mat3(v1, v2, v3);
+	      for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
+		string vertName = (*it).first;
+		Vertex * tempVert = (*it).second;
+		vec3 vertPos = tempVert->getPos();
+		vec3 newPos = rotX * vertPos;
+		double nX, nY, nZ;
+		nX = newPos[0];
+		nY = newPos[1];
+		nZ = newPos[2];
+		tempVert->setPos(nX, nY, nZ);
+	      }
+	    } else if (element.substr(1,2) == "ry") {
+	      double angle;
+	      ss >> angle;
+	      angle = (angle*PI)/180;
+	      vec3 v1 = vec3(cos(angle), 0, sin(angle));
+	      vec3 v2 = vec3(0, 1, 0);
+	      vec3 v3 = vec3(-sin(angle), 0, cos(angle));
+	      mat3 rotY = mat3(v1, v2, v3);
+	      for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
+		string vertName = (*it).first;
+		Vertex * tempVert = (*it).second;
+		vec3 vertPos = tempVert->getPos();
+		vec3 newPos = rotY * vertPos;
+		double nX, nY, nZ;
+		nX = newPos[0];
+		nY = newPos[1];
+		nZ = newPos[2];
+		tempVert->setPos(nX, nY, nZ);
+	      }
+	    } else if (element.substr(1,2) == "rz") {
+	      double angle;
+	      ss >> angle;
+	      angle = (angle*PI)/180;
+	      vec3 v1 = vec3(cos(angle), -sin(angle), 0);
+	      vec3 v2 = vec3(sin(angle), cos(angle), 0);
+	      vec3 v3 = vec3(0, 0, 1);
+	      mat3 rotZ = mat3(v1, v2, v3);
+	      for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
+		string vertName = (*it).first;
+		Vertex * tempVert = (*it).second;
+		vec3 vertPos = tempVert->getPos();
+		vec3 newPos = rotZ * vertPos;
+		double nX, nY, nZ;
+		nX = newPos[0];
+		nY = newPos[1];
+		nZ = newPos[2];
+		tempVert->setPos(nX, nY, nZ);
+	      }
+	    }
+	  }
+	}
+	for (map<string, Vertex *>::iterator it = newVertices.begin(); it != newVertices.end(); it++) {
+	  _currentPoly->addVertex((*it).first, (*it).second);
+	}
+	for (map<string, Face *>::iterator it = tempFaces.begin(); it != tempFaces.end(); it++) {
+	  _currentPoly->addFace((*it).first, (*it).second);
+	}
+	_currentGroup->addPolygon(instanceName, _currentPoly);
+	}*/
     } else if (element[1] == 'g') {
       string groupName;
       ss >> groupName;
